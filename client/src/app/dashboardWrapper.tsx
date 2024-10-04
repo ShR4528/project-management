90e29906a33d45af9b8e77e5a2f04dc4a688d3bd
Shr4528 <shr4528@gmail.com>
2024-10-04 16:06:16 -0500

    dashboardwrapper

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardWrapper = ({children } :{children: React.ReactNode } ) => {
+  return (
+    <div className='flex min-h-screen w-full bg-gray text-gray-900'>dashboardWrapper</div>
+  )
+}
+
+export default DashboardWrapper
