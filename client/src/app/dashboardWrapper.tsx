f5625bd804dd8a62690a585a89bb0ae42d3f4149
Shr4528 <shr4528@gmail.com>
2024-10-08 14:39:43 -0500

    sidebarStart

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Navbar from './(components)/Navbar/page'
+import Sidebar from './(components)/Sidebar'
 
-type Props = {}
+
+
 
 const DashboardWrapper = ({children } :{children: React.ReactNode } ) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
       {/* sidebar */}
-      sidebar
+      <Sidebar />
       <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-500`}>
     <Navbar />
     {children}
-      </main>
-      </div>
+    </main>
+  </div>
   )
 }
 
